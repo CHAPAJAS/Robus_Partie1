a76c033abb17c27aedd461b645c34e5466109699
AdrienRL <60350148+AdrienRL@users.noreply.github.com>
2020-10-01 14:46:14 -0400

    Avance droit!

--- ROBUS/src/main.cpp
+++ ROBUS/src/main.cpp
@@ -6,13 +6,13 @@
 
 #define PERIODE  3
 
-#define KPD      17
-#define KID      0.009
-#define KDD      12
+#define KPD      0.00002
+#define KID      0.00003
+//#define KDD      12
 
-#define KPG      17
-#define KIG      0.009
-#define KDG      12
+#define KPG      0.00001
+#define KIG      0.00002
+//#define KDG      12
 
 #define ENCODEUR 3200
 
@@ -46,13 +46,16 @@ float integralG;
 float integralD;
 float deriveeG;
 float deriveeD;
-bool avancementFini=false;
+bool avancementFiniG=false;
+bool avancementFiniD=false;
 
 
 void setup()
 {
   
   BoardInit();
+  ENCODER_ReadReset(0);
+  ENCODER_ReadReset(1);
   valeurEncodeurD = ENCODER_Read(1);
   valeurEncodeurG = ENCODER_Read(0);
   //Print de la valeur des encodeurs au temps 0
@@ -62,27 +65,73 @@ void setup()
   pinMode(26, INPUT);
   cmdG =AvancerTest;
   cmdD =AvancerTest;
-  //Roues commencent à avancer
-  MOTOR_SetSpeed(0, 0.1);
-  MOTOR_SetSpeed(1, 0.1);
   //Timer1.initialize(500);
 }
 
 void loop()
 {
 
-  valeurEncodeurG = ENCODER_Read(0);
-  valeurEncodeurD = ENCODER_Read(1);
-  while(avancementFini)
+  
+  while((!avancementFiniG)&&(!avancementFiniD))
   {
+    valeurEncodeurG = ENCODER_Read(0);
+    valeurEncodeurD = ENCODER_Read(1);
     avancer(cmdG,cmdD);
     PIDG();
     PIDD();
-  }
-  avancementFini=false;
-  
-  
 
+  }
+  avancementFiniG=false;
+  avancementFiniD=false;
+
+
+  while(1)
+  {}
+}
+
+void PIDG()
+{
+  if (millis()-timer >=PERIODE)
+  {
+
+    retroactionPrecG = retroactionG;
+    retroactionG = valeurEncodeurG;
+    propG = (consigneG-retroactionG)*KPG;
+  
+  
+  timer= millis();
+  erreurG=consigneG-retroactionG;
+
+  //deriveeG=(retroactionG-retroactionPrecG)*KDG;
+  integralG +=erreurG *KIG;
+  //integralG=constrain(integralG+KIG*integralG,-255,255);//Verifier les vrais bornes mon ami!!!
+  cmdG=propG+KIG*integralG;
+  }
+  if((erreurG<0.5)&&(erreurG>-0.5))
+  {
+    avancementFiniG =true;
+  }
+
+}
+void PIDD()
+{
+  if (millis()-timer >=PERIODE)
+  {
+    retroactionPrecD = retroactionD;
+    retroactionD = valeurEncodeurD;
+    propD = (consigneD-retroactionD)*KPD;
+    timer=millis();
+    erreurD=consigneD-retroactionD;
+
+  //deriveeD=(retroactionD-retroactionPrecD)*KDD;
+  integralD +=erreurD *KID;
+ // integralD=constrain(integralD+KID*integralD,-255,255);//Verifier les vrais bornes mon ami!!!
+  cmdD=propD+KID*integralD;
+  }
+  if((erreurD<0.5)&&(erreurD>-0.5))
+  {
+    avancementFiniD =true;
+  }
 
 }
 
@@ -90,8 +139,11 @@ void avancer(int longueurCMG,int longueurCMD)
 {
   
     int valeurEncodeurG = ENCODER_Read(0);
+    int valeurEncodeurD = ENCODER_Read(1);
     print("Valeur encodeur : %ld\n", valeurEncodeurG);
     //si la valeur lue par l'encodeur >= à distance à parcourir en valeur des encodeurs
+    MOTOR_SetSpeed(0, 0.5);
+    MOTOR_SetSpeed(1, 0.5);
     if(valeurEncodeurG >= (ENCODEUR/(PI * DIAMETRE_ROUE))* longueurCMG)
     {
       MOTOR_SetSpeed(0, 0);
@@ -106,44 +158,3 @@ void avancer(int longueurCMG,int longueurCMD)
     }
   
 }
-void PIDG()
-{
-  if (millis()-timer >=PERIODE)
-  {
-    retroactionPrecG = retroactionG;
-    retroactionG = valeurEncodeurG;
-    propG = (consigneG-retroactionG)*KPG;
-  }
-  erreurG=consigneG-retroactionG;
-
-  deriveeG=(retroactionG-retroactionPrecG)*KDG;
-  integralG +=erreurG *KIG;
-  integralG=constrain(integralG+KIG*integralG,-255,255);//Verifier les vrais bornes mon ami!!!
-  cmdG=propG+KIG*integralG-deriveeG;
-  if((erreurG<0.5)&&(erreurG>-0.5))
-  {
-    avancementFini =true;
-  }
-
-}
-void PIDD()
-{
-  if (millis()-timer >=PERIODE)
-  {
-    retroactionPrecD = retroactionD;
-    retroactionD = valeurEncodeurD;
-    propD = (consigneD-retroactionD)*KPD;
-  }
-  erreurD=consigneD-retroactionD;
-
-  deriveeD=(retroactionD-retroactionPrecD)*KDD;
-  integralD +=erreurD *KID;
-  integralD=constrain(integralD+KID*integralD,-255,255);//Verifier les vrais bornes mon ami!!!
-  cmdD=propD+KID*integralD-deriveeD;
-  if((erreurD<0.5)&&(erreurD>-0.5))
-  {
-    avancementFini =true;
-  }
-
-}
-
