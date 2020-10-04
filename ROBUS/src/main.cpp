669e4681a8af40129f89c257c572247a3ff46aec
HermBB <39308818+HermBB@users.noreply.github.com>
2020-10-04 15:53:24 -0400

    Robus A 360 degré

--- ROBUS/src/main.cpp
+++ ROBUS/src/main.cpp
@@ -1,107 +1,83 @@
 #include <Arduino.h>
 #include "LibCHAPAJAS.h"
-#include "LibCHAPAJAS.cpp"
 
-#define ENCODEUR_GAUCHE 3145
-#define ENCODEUR_DROIT  2692
+#define ENCODEUR_GAUCHE_360 (long)8169
+#define ENCODEUR_DROIT_360  (long)7667
+#define DIAMETRE_ROUE      (3 * 2.54)
+#define DIAMETRE_TOUR      18.5
 
-#define DIAMETRE_ROUE     7.62
-#define DIAMETRE_TOUR      7.8
 
-#define PI              3.1415926535
 //void avancer(int longueurCM);
-
+void Virage_Gauche();
 void Virage_Droit();
+//void Virage_Gauche(float valeurDesireeCM);
+//void VirageGrandRayonGauche(int distanceCMG, float rapportD);
+//float CMtoCoche(float ValeurCM);
 
-void setup() {
+
+
+
+void setup()
+{
   BoardInit();
-  
+  delay(1500);
+
 }
 
-int conteur = 0;
+//int conteur = 0;
 
-void loop() {
 
+void loop()
+{
   Virage_Droit();
-  conteur = conteur + 1;
+  delay(2000);
+  Virage_Gauche();
+  //conteur = conteur + 1;
 
-  if (conteur >= 3)
+  //if (conteur >= 1)
   {
     while(true){}
   }
 
-  //avancer(17);
-
-  // if (digitalRead(26) == HIGH)
-  // {
-  //   MOTOR_SetSpeed(0, 0);
-  //   MOTOR_SetSpeed(1, 0);
-
-  //   print("Encodeur 0: %ld\n",ENCODER_Read(0));
-  //   print("Encodeur 1: %ld\n",ENCODER_Read(1));
-  //   while(true)
-  //   {}
-  // }  
 }
 
-/*void avancer(int longueurCM)
+
+
+void Virage_Droit() 
 {
-  while(true)
+
+  ENCODER_ReadReset(0);
+  ENCODER_ReadReset(1);
+
+  long valeurEncodeurGauche = ENCODER_Read(0);
+  
+  while(valeurEncodeurGauche <= ENCODEUR_GAUCHE_360) 
   {
-
-    int valeurEncodeur = ENCODER_Read(0);
-    print("Valeur encodeur : %ld\n", valeurEncodeur);
-    //si la valeur lue par l'encodeur >= à distance à parcourir en valeur des encodeurs
-    if(valeurEncodeur >= (ENCODEUR_GAUCHE/(PI * DIAMETRE_ROUE))* longueurCM)
-    {
-      MOTOR_SetSpeed(0, 0);
-      MOTOR_SetSpeed(1, 0);
-      while(true)
-      {}
-    }
+      MOTOR_SetSpeed(0, 0.3);
+      MOTOR_SetSpeed(1, -0.3);
+      valeurEncodeurGauche = ENCODER_Read(0);
   }
-}*/
 
-void Virage_Droit()
-{
-
-
-
-  /*MOTOR_SetSpeed(0, 0.3);
-  MOTOR_SetSpeed(1, -0.3);
-  delay(3000);
   MOTOR_SetSpeed(0, 0);
   MOTOR_SetSpeed(1, 0);
-  delay(3000);
-*/
-  int valeurEncodeurGauche = ENCODER_Read(0);
-  int valeurEncodeurDroit = ENCODER_Read(1);
-  float DistanceParcouru = (ENCODEUR_GAUCHE/(PI * DIAMETRE_ROUE))*((DIAMETRE_TOUR * PI)/2);
-
-  if (DistanceParcouru >= valeurEncodeurGauche)
-  {
-    MOTOR_SetSpeed(0, 0.3);
-    MOTOR_SetSpeed(1, -0.3);
-    Serial.println("allo");
-    Serial.println(valeurEncodeurGauche);
-    Serial.println(valeurEncodeurDroit);
-    Serial.println(DistanceParcouru);
-  }
-  else
-  { 
-    ENCODER_ReadReset(0);
-    ENCODER_ReadReset(1);
-    MOTOR_SetSpeed(0, 0);
-    MOTOR_SetSpeed(1, 0);
-    while(true)
-    {}
-  }
-  
-  
-  
-
-  
-  
- 
-
 }
+
+void Virage_Gauche() 
+{
+
+  ENCODER_ReadReset(0);
+  ENCODER_ReadReset(1);
+
+  long valeurEncodeurDroit = ENCODER_Read(1);
+  
+  while(valeurEncodeurDroit <= ENCODEUR_DROIT_360) 
+  {
+      MOTOR_SetSpeed(0, -0.3);
+      MOTOR_SetSpeed(1, 0.3);
+      valeurEncodeurDroit = ENCODER_Read(1);
+  }
+
+  MOTOR_SetSpeed(0, 0);
+  MOTOR_SetSpeed(1, 0);
+}
+
