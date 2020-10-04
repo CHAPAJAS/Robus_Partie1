e56f8b1d920c7d9aa5c29b0d00b59f2dc15f41d5
Joey <giuseppe.lomonaco@usherbrooke.ca>
2020-10-04 16:53:23 -0400

    Sequence parcours

--- ROBUS/src/main.cpp
+++ ROBUS/src/main.cpp
@@ -1,49 +1,27 @@
 #include <Arduino.h>
 #include "LibCHAPAJAS.h"
-#include "LibCHAPAJAS.cpp"
+
 
 #define ENCODEUR_GAUCHE 3145
 #define ENCODEUR_DROIT  2692
 
 #define DIAMETRE_ROUE     7.62
 
-#define PI              3.1415926535
 
 
-
-
-
-typedef struct    // Une structure est plusieurs donn/es mis dans un paquet qui contient toutes ces donnees
+typedef struct    // Une structure est plusieurs donnees mis dans un paquet qui contient toutes ces donnees
                   // Un peu comme une classe sans fonction
 {
 
-  float angle;
-  float longueur;
+  double angle;
+  double longueur;
 
 } Vecteur;
 
 
 
-/*
-Vecteur v1, v2, v3, v4, v5 ;    //creer 5 nouveaux vecteurs 
-*/
-
-
-
 // Ici les vecteur sont de la forme (angle, longueur)
-Vecteur tab[5] = { {0,5}, {45,12}, {180,0}, {0,12}, {-45,5} };    //creer 5 nouveaux vecteurs mais dans un tableau
-
-
-
-
-
-
-
-
-
-
-
-
+Vecteur tab[] = { {0,50}, {45,120}, {180,0} };    //creer 5 nouveaux vecteurs mais dans un tableau
 
 
 
@@ -52,32 +30,47 @@ Vecteur tab[5] = { {0,5}, {45,12}, {180,0}, {0,12}, {-45,5} };    //creer 5 nouv
 void avancer(int longueurCM);
 
 
+
+void tournerTest(double angle);
+void avancerTest(double longueurCm);
+
+void sequenceParcours()
+{
+  for (int i = 0; i < sizeof_array(tab); i++)
+  {
+      Vecteur a = tab[i];
+      tournerTest(a.angle);
+      avancerTest(a.longueur);
+  }
+
+  for (int i = sizeof_array(tab) - 2; i >= 0; i--)
+  {
+      Vecteur a = tab[i];
+      avancerTest(a.longueur);
+      tournerTest((-1) * a.angle);
+  }
+}
+
+
+void tournerTest(double angle)
+{
+  print("Virage à %d\n", (int)angle);
+}
+
+void avancerTest(double longueurCm)
+{
+  print("Déplacement de %d cm\n", (int)longueurCm);
+}
+
+
 void setup() {
   BoardInit();
-  //Print de la valeur des encodeurs au temps 0
-  print("Encodeur 0: %ld\n",ENCODER_Read(0));
-  print("Encodeur 1: %ld\n",ENCODER_Read(1));
-  //Activation du bumper avant
-  pinMode(26, INPUT);
-  //Roues commencent à avancer
-  MOTOR_SetSpeed(0, 0.1);
-  MOTOR_SetSpeed(1, 0.1);
+  sequenceParcours();
 }
+
 
 void loop() {
 
-  avancer(17);
-
-  // if (digitalRead(26) == HIGH)
-  // {
-  //   MOTOR_SetSpeed(0, 0);
-  //   MOTOR_SetSpeed(1, 0);
-
-  //   print("Encodeur 0: %ld\n",ENCODER_Read(0));
-  //   print("Encodeur 1: %ld\n",ENCODER_Read(1));
-  //   while(true)
-  //   {}
-  // }  
 }
 
 void avancer(int longueurCM)
